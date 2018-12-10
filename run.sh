c2c5773eacaf6842f1146bd33ecd751daeff53eb
notacoder69 <aritro.paul61@gmail.com>
2018-12-10 16:12:36 +0530

    Initial Commit

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+FILENAME=$(basename -- "$1")
+#echo ${FILENAME}
+FILE=$(find . -name "${FILENAME}.*")
+EXT="${FILE##*.}"
+
+case "$EXT" in
+        c)  gcc ${FILE} -o ${FILENAME}
+            ./${FILENAME}.out
+           ;;
+        py) python3 ${FILE}
+           ;;
+        sh) sh ${FILE}
+            ;;
+        cpp) g++ ${FILE} -o ${FILENAME}
+            ./${FILENAME}.out
+             ;;
+        java) javac ${FILE}
+              java ${FILENAME}
+             ;;
+        txt) echo "$filename : Text file"
+             ;;
+        *) echo " $filename : Not processed"
+           ;;
+esac
